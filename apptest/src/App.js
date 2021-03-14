f27c141bb7b0c88d2276206c6e15fd0e998b7b29
Wanderson Oliveira <wanderson1873@live.com>
2021-03-14 10:00:03 +0000

    filter and map

--- apptest/src/App.js
+++ apptest/src/App.js
@@ -1,45 +1,46 @@
 import React from 'react';
 
-// Mostre os dados da aplicação, como aprensetado no vídeo
-// Não utilize CSS externo, use o style para mudar as cores
-// Se a situação estiver ativa pinte de verde, inativa vermelho
-// Se o gasto for maior que 10000 mostre uma mensagem
-
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 
 const App = () => {
-  const luana = {
-    cliente: 'Luana',
-    idade: 27,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-    ],
-    ativa: true,
-  };
-  
-  const mario = {
-    cliente: 'Mario',
-    idade: 31,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-      { nome: 'Guitarra', preco: 'R$ 3500' },
-    ],
-    ativa: false,
-  };
-  const dados = mario;
+  const dados = produtos.filter(
+    ({preco}) => Number(preco.replace("R$ ", "")) > 1500)
+  console.log(dados)
 
-  const total = dados.compras.map((item) => Number(item.preco.replace("R$ ", ""))).reduce((a, b) => a +b)
-  console.log(total)
-  return (<div>
-    <p>Nome: {dados.cliente}</p>
-    <p>Idade: {dados.idade}</p>
-    <p>Situacao: <span style={{color: dados.ativa ? 'green' : 'red'}}> {dados.ativa ? 'Ativo' : 'Inativo'}</span></p>
-    <p>Total gasto R$ {total}</p>
-    <p>{total > 10000 && " você está gastando muito!"}</p>
-  </div>);
+  return (
+  <>
+    {dados.map(({id, preco, nome, cores}) => (
+      <div key={id}>
+        <h1>{nome}</h1>
+        <p>Preço: {preco}</p>
+        <ul>
+          {cores.map((cor) => (
+            <li style={{ backgroundColor: cor, color: 'white' }} key={cor}>{cor}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </>
+  )
 };
 
 export default App
