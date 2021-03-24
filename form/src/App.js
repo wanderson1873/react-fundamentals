e8a84eb5b35ccd18a541cc4b81a1c5d5fbeff11a
Wanderson Oliveira <wanderson1873@live.com>
2021-03-24 21:25:46 +0000

    radio

--- form/src/App.js
+++ form/src/App.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import { Input } from './Form/Input'
+import Radio from './Form/Radio'
 import Select from './Form/Select'
 
 const App = () => {
   const [name, setName] = React.useState('')
   const [email, setEmail] = React.useState('')
   const [produto, setProduto] = React.useState('')
+  const [cor, setCor] = React.useState('')
 
   return (
     <form>
+      <Radio 
+        options={["Azul", "Vermelho", "Laranja"]}
+        value={cor}
+        setValue={setCor}
+      />
+
       <Select 
         options={["notebook", "tablet", "smartphone"]}
         value={produto}
         setValue={setProduto}
       />
-      
+
       <Input id="name" label="Nome" value={name} setValue={setName} />
       <Input 
         id="email" 
