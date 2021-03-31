20c90a6d3224da2994468a227da4a43af1bdeab7
Wanderson Oliveira <wanderson1873@live.com>
2021-03-31 20:01:07 +0100

    Update Header.js

--- ecommerce/src/Components/Header.js
+++ ecommerce/src/Components/Header.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
 import style from './Header.module.css'
 
 const Header = () => {
    return (
-      <div>
-         <h1>Header</h1>
-      </div>
+      <nav className={style.header}>
+         <ul>
+            <li>
+               <NavLink
+               activeClassName={style.active} className={style.link} to="/" end>
+                  Produtos
+               </NavLink>
+            </li>
+            <li>
+               <NavLink 
+               activeClassName={style.active} className={style.link} to="/contato">
+                  Contato
+               </NavLink>
+            </li>
+         </ul>
+      </nav>
    )
 }
 
