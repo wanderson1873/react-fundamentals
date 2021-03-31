887f6611c17f9c172277c2ac9893b6271a032b11
Wanderson Oliveira <wanderson1873@live.com>
2021-03-31 18:54:29 +0100

    Create pages

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,12 +1,23 @@
-const { BrowserRouter, Routes, Route } = require("react-router-dom");
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Produtos from './Components/Produtos'
+import Contato from './Components/Contato'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="" component={} />
-        </Routes>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" exact element={<Produtos />} />
+            <Route path="/contato" exact element={<Contato />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ ecommerce/src/Components/Contato.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Contato.module.css'
+
+const Contato = () => {
+   return (
+      <div>
+         <h1>Contato</h1>
+      </div>
+   )
+}
+
+export default Contato
