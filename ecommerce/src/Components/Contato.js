e99bdd59335be3114ba6f83d8adcfd73dcf46583
Wanderson Oliveira <wanderson1873@live.com>
2021-03-31 20:01:03 +0100

    Update Contato.js

--- ecommerce/src/Components/Contato.js
+++ ecommerce/src/Components/Contato.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import Head from './Head'
 import style from './Contato.module.css'
+import foto from '../img/contato.jpg'
 
 const Contato = () => {
    return (
-      <div>
-         <h1>Contato</h1>
-      </div>
+      <section className={style.contato + " animeLeft"}>
+         <Head title="Loja | Contato"  description="Entre em contato com a nossa loja"/>
+         <img src={foto} alt="Maquina de escrever"/>
+
+         <div>
+            <h1>Entre em contato</h1>
+            <ul className={style.dados}>
+               <li>wanderson@loja.com</li>
+               <li>9999-9999</li>
+               <li>Rua Ali Perto, 46</li>
+            </ul>
+         </div>
+         
+      </section>
    )
 }
 
