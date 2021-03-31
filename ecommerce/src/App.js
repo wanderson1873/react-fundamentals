aa37570471a84243f25e656eaf66233e0d8ebe6a
Wanderson Oliveira <wanderson1873@live.com>
2021-03-31 18:18:29 +0100

    new project

---
+++ ecommerce/src/App.js
@@ -0,0 +1,15 @@
+const { BrowserRouter, Routes, Route } = require("react-router-dom");
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="" component={} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
